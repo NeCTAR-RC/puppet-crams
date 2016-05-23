2a50d2ae8c8ebc2ecd924dd078d7f08d79ac08f1
Rafi M Feroze <mohamed.feroze@monash.edu>
2016-05-23 13:51:50 +1000

    Add scripts for provisioning (cramsclient-nectar) Add missing params to crams-api class init crams-api manifest and templates
    Change-Id: If74632dc654663c7c9db2392064097c9581db846

---
+++ manifests/api.pp
@@ -0,0 +1,30 @@
+# Crams API server install and configuration
+class crams::api (
+  $crams_email_address=$::fqdn,
+  $crams_env,
+  $ks_admin_user,
+  $ks_admin_pass,
+  $ks_admin_tenant,
+  $ks_auth_url,
+  $allowed_hosts=[],
+  $secret_key,
+  $db_name='crams',
+  $db_username='crams',
+  $db_password,
+  $db_host='localhost',
+  $db_port='',
+  $admin_email='root@localhost') inherits crams
+{
+
+  package { 'crams-api':
+    ensure => installed,
+  }
+
+  file { '/etc/crams/settings.py':
+    owner   => root,
+    group   => root,
+    mode    => '0600',
+    content => template('crams/api/settings.erb'),
+    require => Package['crams-api'],
+  }
+}
