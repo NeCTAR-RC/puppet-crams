1bf1f8f124bf1778b6f1a884fdf337970ebb7133
Rafi M Feroze <mohamed.feroze@monash.edu>
2016-05-23 13:51:50 +1000

    init crams-api manifest and templates
    Change-Id: If74632dc654663c7c9db2392064097c9581db846

---
+++ manifests/api.pp
@@ -0,0 +1,19 @@
+# Crams API server install and configuration
+class crams::api (
+  $host='localhost',
+  $port=80,
+  $admin_email='root@localhost') inherits crams
+{
+    
+  package {'crams-api':
+    ensure => installed,
+  }
+  
+  file {'/etc/crams/settings.py':
+    owner   => root,
+    group   => root,
+    mode    => '0600',
+    content => template('crams/api/settings.erb'),
+    require => Package['crams-api'],
+  }
+}
