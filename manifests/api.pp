b1fec930fef117c4984e9a689c36b18f988d2807
Rafi M Feroze <mohamed.feroze@monash.edu>
2016-05-23 13:51:50 +1000

    Fix alignment warning errors Add all variables declared in templates to Class context update admin email to configurable params fix all comments
    init crams-api manifest and templates
    
    Change-Id: If74632dc654663c7c9db2392064097c9581db846
    
    align => to as close to param as possible, max four spaces
    
    Change-Id: I4c3064cc53d38fe7d7ed5522c59cabaa058c8563
    
    align => that did not work, so use single space instead
    
    Change-Id: I84ca7285acb3adacb86675d7f02692fba707693e
    
    align => missed out a few spaces
    
    Change-Id: I9dc8e8dfe09dcf7206281f660b935ae78946cc2f
    
    align => fix space errors, try again, single space and new line tab
    
    Fix some comments
    - need help configuring smtp server and using $environment variable
    
    Change-Id: If74632dc654663c7c9db2392064097c9581db846

---
+++ manifests/api.pp
@@ -0,0 +1,78 @@
+# Crams API server install and configuration
+  class crams::api (
+    $host='localhost',
+    $port=80,
+    $crams_email_address='root@localhost',
+    $environment,
+    $ks_admin_user,
+    $ks_admin_pass,
+    $ks_admin_tenant,
+    $ks_auth_url,
+    $allowed_hosts,
+    $secret_key,
+    $db_name,
+    $db_username,
+    $db_password,
+    $db_host,
+    $db_port,
+  ) inherits crams {
+
+  package {'crams-api':
+    ensure => installed,
+  }
+
+  class { 'apache::mod::wsgi': }
+
+  file {'/etc/crams/settings.py':
+    owner   => root,
+    group   => root,
+    mode    => '0600',
+    content => template('crams/api/settings.erb'),
+    require => Package['crams-api'],
+  }
+
+  apache::vhost {
+    $host:
+      serveradmin => $crams_email_address,
+      port => $port,
+      docroot => '/var/lib/www',
+      fallbackresource => '/usr/share/crams/static/severe-error.html',
+
+      wsgi_application_group => '%{GLOBAL}',
+      wsgi_daemon_process => 'wsgi',
+      wsgi_daemon_process_options => {
+        processes => '2',
+        threads => '15',
+        display-name => '%{GROUP}',
+      },
+      wsgi_import_script => '/usr/share/crams/wsgi/crams.wsgi',
+      wsgi_import_script_options => {
+        process-group => 'wsgi',
+        application-group => '%{GLOBAL}',
+      },
+      wsgi_process_group => 'wsgi',
+      wsgi_script_aliases => {
+        '/' => '/usr/share/crams/wsgi/crams.wsgi'
+      },
+      aliases => [
+        {
+          alias => '/media',
+          path => '/usr/share/crams/media',
+        },
+        {
+          alias => '/static',
+          path => '/usr/share/crams/static',
+        },
+      ],
+      directories => [
+        {
+          path => '/usr/share/crams/media',
+          options => ['-Indexes']
+        },
+        {
+          path => '/usr/share/crams/static',
+          options => ['-Indexes']
+        },
+      ],
+  }
+  }
