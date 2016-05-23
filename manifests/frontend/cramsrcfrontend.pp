787333a4369934e870cb3b4e649a0dd25d0907d6
Rafi M Feroze <mohamed.feroze@monash.edu>
2016-05-23 13:51:50 +1000

    Add scripts for provisioning (cramsclient-nectar) Add missing params to crams-api class init crams-api manifest and templates Add frontend puppet scripts
    Change-Id: If74632dc654663c7c9db2392064097c9581db846

---
+++ manifests/frontend/cramsrcfrontend.pp
@@ -0,0 +1,44 @@
+class crams::frontend::cramsrcfrontend(
+  $cramsapi_server,
+  $servername          = $::fqdn,
+  $port                = 443,
+  $bind_host           = undef,
+  $ssl                 = true,
+  $ssl_cert            = undef,
+  $ssl_key             = undef,
+  $ssl_chain           = undef,
+  $ssl_ca              = undef,
+  $ssl_certs_dir       = undef,
+  $crams_frontend_root = '/var/www/app'
+
+) inherits crams{
+
+  include ::apache
+
+  if $ssl {
+    include ::apache::mod::ssl
+  }
+
+  package { 'crams-rc-frontend':
+    ensure => installed,
+  }
+
+  file { '/var/www/app':
+    ensure  => 'link',
+    target  => '/usr/share/crams-rc-frontend/app',
+    require => Package['crams-rc-frontend', 'httpd'],
+  }
+
+  ::apache::vhost { $servername:
+    ensure         => 'present',
+    servername     => $servername,
+    port           => $port,
+    ip             => $bind_host,
+    docroot        => $crams_frontend_root,
+    ssl            => $ssl,
+    ssl_cert       => $ssl_cert,
+    ssl_key        => $ssl_key,
+    ssl_chain      => $ssl_chain,
+    ssl_certs_dir  => $ssl_certs_dir,
+  }
+}
