1f6e608511f1550b4fbe8904ee1198096f55d746
Simon Yu <xiaoming.yu@monash.edu>
2016-08-05 11:25:38 +1000

    added logging conf settings in the template
    Change-Id: I555784d46a7c50651ffa9c7a2e32505f580db502

--- manifests/provisioner/nectar.pp
+++ manifests/provisioner/nectar.pp
@@ -3,10 +3,13 @@ class crams::provisioner::nectar(
   $cramsapi_auth_path='json_token_auth',
   $crams_provision_api_path='api/v1/provision_project/list',
   $crams_provision_api_update_path='api/v1/provision_project/update',
+  $provision_log_dir = '/var/log/cramsclient-nectar',
   $os_auth_url,
   $os_project_name,
   $os_username,
   $os_password,
+  $crams_user='crams',
+  $crams_group='crams',
   $cron_hour='*',
   $cron_minute='*/30'
 ) inherits crams{
@@ -15,17 +18,32 @@ class crams::provisioner::nectar(
     ensure => installed,
   }
 
+  user { $crams_user:
+    ensure => 'present',
+  }
+
+  group { $crams_group:
+    ensure => 'present',
+  }
+
+  file { $provision_log_dir:
+    ensure => 'directory',
+    owner  => $crams_user,
+    group  => $crams_group,
+    mode   => '0750',
+  }
+
   file { '/etc/cramsclient-nectar/settings.py':
-    owner   => root,
-    group   => root,
+    owner   => $crams_user,
+    group   => $crams_group,
     mode    => '0644',
     content => template('crams/provisioner/nectar/setting.py.erb'),
     require => Package['cramsclient-nectar'],
   }
 
   cron { 'crams-provision-accounts':
-    command => '/usr/bin/crams-provision-nectar > /var/log/cramsclient-nectar/provision.log 2>&1',
-    user    => root,
+    command => "/usr/bin/crams-provision-nectar > ${$provision_log_dir}/provision.log 2>&1",
+    user    => $crams_user,
     hour    => $cron_hour,
     minute  => $cron_minute,
     require => Package['cramsclient-nectar'],
